2809d65124f3505a1b44d9852a51acdd69aa9573
vladiatorr <31032721+vladiatorr@users.noreply.github.com>
2017-09-20 19:56:08 +0200

    t

--- js/onload.js
+++ js/onload.js
@@ -11,9 +11,7 @@ window.addEventListener('load', function() {
   } else {
     swal({
       title : 'Oops...',
-      html :  '<h4>My usual CV (.pdf) is available <a href="https://pavlovdog.github.io/potekhin_sergey.pdf">here</a></h4>' +
-              '<p align="left">This web page was build with <a href="https://www.ethereum.org/" target="_blank">Ethereum platform</a> and at seems like your browser doesn\'t support it yet!</p>' +
-              '<p align="left">If you still want to use this page, please, make sure that:</p>' +
+      html :  '<p align="left">If you still want to use this page, please, make sure that:</p>' +
               '<ul align="left">' +
               '<li>You\'re using <a href="https://www.google.com/chrome/" target="_blank">Chrome browser</a></li>' +
               '<li>You have installed <a href="https://metamask.io/" target="_blank">MetaMask plugin</a></li>' +
